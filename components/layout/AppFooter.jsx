2ccc7d51737b5748bfe18c11c730040840f8a9bc
kosadchij <kosadchij@inbox.ru>
2020-02-12 01:03:10 +0300

    changed readme

--- components/layout/AppFooter.jsx
+++ components/layout/AppFooter.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Layout } from 'antd';
+import Link from 'next/link';
 
 const { Footer } = Layout;
 
 export default function AppFooter() {
   return (
     <Footer style={{ textAlign: 'center' }}>
-      Pizza-App © {new Date().getFullYear()} Created by Kosadchiy
+      Pizza-App © {new Date().getFullYear()} Created by <Link href="https://github.com/Kosadchiy"><a>Kosadchiy</a></Link>
     </Footer>
   );
 }
